d2c30ba687474256f3b3af9bcfb6b62cba3b2456
ivanawetek <ivana.kovacevic@wetek.com>
2020-09-13 11:20:08 +0200

    controller

---
+++ GitHubReposWebApplication/Controllers/ReposController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Contracts;
+using GitHubReposWebApplication.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHubReposWebApplication.Controllers
+{
+    [Route("api/repos")]
+    [ApiController]
+    public class ReposController : ControllerBase
+    {
+    }
+}
