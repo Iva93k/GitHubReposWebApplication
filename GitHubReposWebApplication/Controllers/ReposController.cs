6a969ec33edef2f797191a88a4946464b4559ec1
ivanawetek <ivana.kovacevic@wetek.com>
2020-09-13 15:30:38 +0200

    Fetching external API

--- GitHubReposWebApplication/Controllers/ReposController.cs
+++ GitHubReposWebApplication/Controllers/ReposController.cs
@@ -1,18 +1,63 @@
 ﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Contracts;
-using GitHubReposWebApplication.Constants;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GitHubReposWebApplication.Controllers
 {
-    [Route("api/repos")]
+    //[Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class ReposController : ControllerBase
     {
+        private ILoggerManager _logger;
+        public ReposController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetRepository()
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("https://api.github.com");
+                    client.DefaultRequestHeaders.Add("User-Agent", "C# App");
+                    var response = await client.GetAsync($"/repos/Iva93k/employee-timesheet");
+
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    
+                    var rawRepo = JsonConvert.DeserializeObject<ReposResponse>(stringResult);
+
+                    _logger.LogInfo($"Returned all informations.");
+
+                    return Ok(new
+                    {
+                        RepositoryName          = rawRepo.Name,
+                        RepositoryFullName      = rawRepo.Full_name,
+                        RepositoryPrivate       = rawRepo.Private,
+                        RepositoryHtmlUrl       = rawRepo.Html_url,
+                        RepositoryDescription   = rawRepo.Description,
+                        RepositoryLanguage      = rawRepo.Language
+
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    _logger.LogError($"Something went wrong: {httpRequestException.Message}");
+                    return BadRequest($"Error getting repository from GitHub: {httpRequestException.Message}");
+                }
+            }
+        }
     }
 }
---
+++ GitHubReposWebApplication/ReposResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubReposWebApplication
+{
+    public class ReposResponse
+    {
+        public string Name { get; set; }
+        public string Full_name { get; set; }
+        public bool Private { get; set; }
+        public string Html_url { get; set; }
+        public string Description { get; set; }
+        public string Language { get; set; }
+
+    }
+}
